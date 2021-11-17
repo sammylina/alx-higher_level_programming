9151d5feff8f12ae224a3695c49e4abff0becaa3
sammyALX <samuelhailu49@gmail.com>
2021-11-16 16:38:25 -0800

    change sring to upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        if islower(c):
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{:s}".format(c), end="")
+    for i, c in enumerate(str):
+        if ord(c) > 96 and ord(c) < 123:
+            str = str[:i] + chr(ord(c) - 32) + str[i + 1:]
+    print("{:s}".format(str), end="")
     print()
-
-
-def islower(c):
-    if ord(c) < 123 and ord(c) > 91:
-        return True
-    else:
-        return False
