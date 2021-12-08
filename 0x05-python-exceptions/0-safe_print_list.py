1c7826c99061989e61d42b9d50431f167c21ab18
SammyAlx <samuelhailu49@gmail.com>
2021-12-08 17:32:27 +0300

    fix nameError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     while True and index < x:
         try:
             print(my_list[index], end="")
-            i++
+            index+=1
         except IndexError:
             break
     print()
