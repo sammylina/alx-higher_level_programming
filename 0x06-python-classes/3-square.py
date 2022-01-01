501c33c70e7387768599c2232e5001f557aec274
SammyAlx <samuelhailu49@gmail.com>
2022-01-01 12:57:35 +0300

    calcaulate are of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """This class represents a Sqaure.
+
+    Atttibutes:
+        __size (int): size of the sqaure.
+        must be >= 0, default value is 0
+    """
+
+    def __init__(self, size=0):
+        """initaialize sqaure with positive value
+
+        Args:
+            size (int): size of sqaure from user
+
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size of < 0
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculate area of the square
+
+        Returns:
+            current square area
+        """
+        area = self.__size * self.__size
+        return area
