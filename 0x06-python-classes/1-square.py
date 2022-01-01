f3dc658ee1ed48bcd8192fa3901e5fc22b7498d7
SammyAlx <samuelhailu49@gmail.com>
2022-01-01 11:33:13 +0300

    add size attribute to the Square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A module that represents a square"""
+
+
+class Square:
+    """Square class with few attributes
+    Attributes:
+        __size (None): size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
