3f54173a7780a9a8dbe4573f230e3b0a012046d2
SammyAlx <samuelhailu49@gmail.com>
2022-01-03 13:32:49 +0300

    print the square using #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """This class represents a Sqaure.
+
+    Atttibutes:
+        __size (int): size of the sqaure.
+        must be >= 0, default value is 0
+    """
+
+    def __init__(self, size=0):
+        """initaialize sqaure with positive value
+
+        Args:
+            size (int): size of sqaure
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get __size attribute of the class"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculate area of the square
+
+        Returns:
+            current square area
+        """
+        area = self.__size * self.__size
+        return area
+
+    def my_print(self):
+        """print area of square using '#' to represent"""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
