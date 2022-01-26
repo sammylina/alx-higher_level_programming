0fd0de61f7ac170f1170a2a53bdd8ef11cb48e01
sammyALX <samuelhailu49@gmail.com>
2022-01-26 16:37:13 +0300

    rewrite 0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    index = 0
-    while True and index < x:
-        try:
-            print(my_list[index], end="")
-            index += 1
-        except IndexError:
-            break
-    print()
-    return index
+    try:
+        count = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except (IndexError):
+        pass
+    finally:
+        print()
+        return count
