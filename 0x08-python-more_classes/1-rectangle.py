77d6c068849cb1ae78f9a5f49351bc69345ad235
sammyALX <samuelhailu49@gmail.com>
2022-01-31 18:25:07 +0300

    Rectangle with height and width properties

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Rectange module
+
+"""
+
+
+class Rectangle:
+    """Rectange that has width and height
+
+    """
+    def __init__(self, width=0, height=0):
+        """Initialize a rectange with positive integer width and height values.
+
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        else:
+            if value >= 0:
+                self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        else:
+            if value >= 0:
+                self.__height = value
