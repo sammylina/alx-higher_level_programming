e9228eced5c076439ddec30f293da9e67a2c3357
sammyALX <samuelhailu49@gmail.com>
2022-02-01 11:56:13 +0300

    change logic syntax in __init__

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
         """Initialize a rectange with positive integer width and height values.
 
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
