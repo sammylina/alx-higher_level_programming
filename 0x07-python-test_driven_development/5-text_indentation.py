106db0c5d65fb051ea9690bd4c24994e2c957ab7
sammyALX <samuelhailu49@gmail.com>
2022-02-02 19:04:20 +0300

    implement text_indentaion and add one more test case

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""text_indentation module
+
+"""
+
+
+def text_indentation(text):
+    """Indent a text using ', ? and : characters and
+    add two newlines following the characters
+
+    Args:
+        text (str): a string text to be indented
+
+    Raises:
+        TypeError: if txt is not a string
+
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    n_text = text.replace('. ', '.\n\n').replace('? ', '?\n\n').\
+        replace(': ', ':\n\n')
+    print(n_text)
