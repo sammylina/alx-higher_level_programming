7e72a6b2534a46c863a497c3af872f7c911505a7
sammyALX <samuelhailu49@gmail.com>
2022-02-05 17:14:59 +0300

    add etst case to max_integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,7 +14,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         max = max_integer([4, 2, 6, 3, 9, 2])
         self.assertEqual(max, 9)
-        max = max_integer([5, 5, 5])
+        max = max_integer([5, 4, 3])
         self.assertEqual(max, 5)
         max = max_integer([-4, -5, -2])
         self.assertEqual(max, -2)
@@ -22,6 +22,8 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max, 6)
         max = max_integer([])
         self.assertEqual(max, None)
+        max = max_integer([8])
+        self.assertEqual(max, 8)
 
         with self.assertRaises(TypeError):
             max_integer(False)
