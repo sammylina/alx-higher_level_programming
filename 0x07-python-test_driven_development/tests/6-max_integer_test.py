29afac782387334d50da8b28f0bff414e07181fb
sammyALX <samuelhailu49@gmail.com>
2022-02-05 14:50:13 +0300

    test  max_integer fun using unittest module

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Maximum integer unit test module
+
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test case class"""
+
+    def test_max_integer(self):
+        max = max_integer([4, 2, 6, 3, 9, 2])
+        self.assertEqual(max, 9)
+        max = max_integer([5, 5, 5])
+        self.assertEqual(max, 5)
+        max = max_integer([-4, -5, -2])
+        self.assertEqual(max, -2)
+        max = max_integer([-3, -1, 6, 0, 4])
+        self.assertEqual(max, 6)
+        max = max_integer([])
+        self.assertEqual(max, None)
+
+        with self.assertRaises(TypeError):
+            max_integer(False)
