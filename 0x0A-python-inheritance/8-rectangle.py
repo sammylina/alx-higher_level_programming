dcb7499b7f3df0af0bdfb5bbe3213f156b3eb0fe
sammyALX <samuelhailu49@gmail.com>
2022-02-07 23:13:55 +0300

    add rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Rectangle module, Inherits from BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Inherits from BaseGeometry class and have width and
+    height.
+
+    Args:
+        width (int): postive integer width of rectangle
+        height (int): positive integer height of rectangle
+    """
+    def __init__(self, width, height):
+        """initialize width and height with positive
+        integers using super().integer_validator
+
+        Args:
+            __width (int): private width value for instance
+            __height (int): private height for the instance
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
