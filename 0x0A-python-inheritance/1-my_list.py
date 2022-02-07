d237c73c947c0af2ef9e7a63802312f265654151
sammyALX <samuelhailu49@gmail.com>
2022-02-07 16:52:48 +0300

    print a list sorted way

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""MyList module. extended version of python list
+"""
+
+
+class MyList(list):
+    """Extended version of python list with
+    ablity to print a sorted list in ascendin order
+
+    Note:
+        we don't need __init__ because the class is
+        inherited from list class
+
+    """
+    def __init__(self, *arg):
+        super().__init__([m for m in arg])
+
+    def print_sorted(self):
+        """print the list in ascending order
+            
+            Attributes:
+                copy (list): copy of the original list
+        """
+        copy = [item for item in self]
+        copy.sort()
+        print(copy)
