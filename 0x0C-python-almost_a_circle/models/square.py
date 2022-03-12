27a17a252ebe054a6760c485f659bbd7f25f82d4
sammyALX <samuelhailu49@gmail.com>
2022-03-12 11:24:08 +0300

    Add update functionality to Sqaure class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -51,3 +51,21 @@ class Square(Rectangle):
         """
         self.width = new_size
         self.height = new_size
+
+    def update(self, *args, **kwargs):
+        """update id, size, x and y value of instance"""
+        if args and len(args):
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.height = args[1]
+                self.width = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+
+        if not args and kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
