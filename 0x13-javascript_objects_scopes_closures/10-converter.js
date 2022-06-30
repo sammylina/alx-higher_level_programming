ce6fd31986ed2f725e6692df9fac415f48d520df
sammylina <88908872+sammylina@users.noreply.github.com>
2022-06-30 15:05:12 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-exports.converter = function (base) {
-  return function (val) {
-    return val.toString(base);
-  };
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.logMe = (function () {
-  let argCount = 0;
-  return function (item) {
-    console.log(`${argCount}: ${item}`);
-    argCount++;
-  };
-})();
