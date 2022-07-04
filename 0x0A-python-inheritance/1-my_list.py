dcb2ba22411845b241ac2b929a47ea80335ef14a
SammyAlx <samuelhailu49@gmail.com>
2022-07-04 15:55:31 +0300

    custom list class with sorting method

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Custom MyList module
+"""
+
+
+class MyList(list):
+    """custom list object that extend the build-in
+    python list object
+
+    """
+
+    def print_sorted(self):
+        """print all items of the list in sorted order
+        """
+        copy = [*self]
+        copy.sort()
+        print(copy)
