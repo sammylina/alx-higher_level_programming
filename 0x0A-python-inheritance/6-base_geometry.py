b6f2897457e31def6ce5fda240b0c3b767e53ebb
SammyAlx <samuelhailu49@gmail.com>
2022-07-06 16:28:18 +0300

    add definition of area method in BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class that declares area method
+    """
+    def area(self):
+        """calculate are of a Geometry
+        """
+        raise Exception('area() is not implemented')
