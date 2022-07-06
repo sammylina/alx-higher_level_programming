b1c6ded237b6eecddc67190d4b85b2b36ae2ba13
SammyAlx <samuelhailu49@gmail.com>
2022-07-06 17:01:40 +0300

    add validation of input to the BaseGeometry class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class that declares area method
+    """
+    def area(self):
+        """calculate are of a Geometry
+        """
+        raise Exception('area() is not implemented')
+    def integer_validator(self, name, value):
+        """validate argument value
+
+        Args:
+            name (string): name of the integer value
+            value (int): integer value 
+
+        Raises:
+            TypeError: if value is not integer type
+        """
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
