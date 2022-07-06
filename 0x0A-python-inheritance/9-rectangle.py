5b2e950646e6be1a2b2e35f331d45740ec7632f8
SammyAlx <samuelhailu49@gmail.com>
2022-07-06 18:01:15 +0300

    implement area method of Rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Rectangle module
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """The Class represents a rectangle shape
+
+    Attributes:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
+
+    def area(self):
+        """calculate are of a rectangle
+
+        Returns:
+            area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__width,
+                                         self.__height)
