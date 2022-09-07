4e02f1b306834ff5bce4069a7a94eed39a507c20
sammyALX <samuelhailu49@gmail.com>
2022-09-07 14:09:03 +0300

    transform an object

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const dict = require('./101-data').dict;
+
+const newDict = {};
+let userId;
+for (const key in dict) {
+  userId = dict[key];
+
+  if (userId in newDict) {
+    newDict[userId].push(key);
+  } else {
+    newDict[userId] = [key];
+  }
+}
+
+console.log(newDict);
