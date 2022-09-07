df8903c12215921d4b8668bcf59f0d092ad6619b
sammyALX <samuelhailu49@gmail.com>
2022-09-07 14:56:41 +0300

    print the second biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const nums = process.argv.slice(2).map(n => parseInt(n));
+
+if (nums.length > 1) {
+  let firstBig = nums[0];
+  let secondBig = nums[0];
+
+  nums.forEach(n => {
+    if (n > firstBig) {
+      secondBig = firstBig;
+      firstBig = n;
+    } else if (n > secondBig && n < firstBig) {
+      secondBig = n;
+    }
+  });
+  console.log(secondBig);
+} else {
+  console.log(0);
+}
