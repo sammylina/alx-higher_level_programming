6406cc43cf917d33630c454bc55ac32169346f38
SammyALX <sammyhailu56@gmail.com>
2022-09-08 14:36:15 -0700

    filter states by their first char

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""filter states using name column"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb as driver
+
+    user, passwd, db = sys.argv[1:]
+    db = driver.connect(host='localhost', port=3306, user=user, passwd=passwd, db=db)
+
+    cur = db.cursor()
+    cur.execute('select * from states where name like \'N%\'')
+
+    for r in cur.fetchall():
+        print(r)
+
+    cur.close()
+    db.close()
