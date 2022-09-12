fdc2ebd4da092ee8ffdb3f054e2b719517b808aa
sammyALX <samuelhailu49@gmail.com>
2022-09-12 11:19:49 +0300

    add case sensetive to filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,10 +6,13 @@ if __name__ == '__main__':
     import MySQLdb as driver
 
     user, passwd, db, state_name = sys.argv[1:]
-    db = driver.connect(host='localhost', port=3306, user=user, passwd=passwd, db=db)
+    db = driver.connect(host='localhost', port=3306,
+                        user=user, passwd=passwd, db=db)
 
     cur = db.cursor()
-    cur.execute("select * from states where name='{:s}' order by id asc".format(state_name))
+    cur.execute("select * from states where \
+                name like binary '{:s}' \
+                order by id asc".format(state_name))
 
     for r in cur.fetchall():
         print(r)
