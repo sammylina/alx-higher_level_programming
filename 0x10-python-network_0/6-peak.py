e50f040131131c8515b217f32a9cbbe720cb15de
sammyALX <samuelhailu49@gmail.com>
2022-11-08 14:24:44 +0300

    add conditional to decide search direction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,13 +6,18 @@
 def find_peak(int_list):
     """Find any peak value in the list
     """
+    print("list: ", int_list)
     if len(int_list) == 0:
         return None
     if len(int_list) == 1:
         return int_list[0]
     else:
         s = (len(int_list) // 2)
+        print("split at: ", s)
         if int_list[s] >= int_list[s-1] and int_list[s] >= int_list[s+1]:
             return int_list[s]
         else:
-            return find_peak(int_list[s:])
+            if int_list[s] <= int_list[s+1]:
+                return find_peak(int_list[s:])
+            else:
+                return find_peak(int_list[:s+1])
