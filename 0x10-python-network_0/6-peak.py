d63ca453c8055b49eb5942580ed15d8856e72c04
sammyALX <samuelhailu49@gmail.com>
2022-11-08 14:44:59 +0300

    remove debugging outuputs

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,14 +6,12 @@
 def find_peak(int_list):
     """Find any peak value in the list
     """
-    print(int_list)
     if len(int_list) == 0:
         return None
-    if len(int_list) == 2:
+    if len(int_list) <= 2:
         return max(int_list)
     else:
         s = (len(int_list) // 2)
-        print("split at: ", int_list[s])
         if int_list[s] >= int_list[s-1] and int_list[s] >= int_list[s+1]:
             return int_list[s]
         else:
