bc30f57304c36258efe03d46a60076c12ca6b6bf
sammyALX <samuelhailu49@gmail.com>
2022-11-08 20:15:04 +0300

    hit end point using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""hbtn_status module
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request as request
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = request.Request(url)
+    with request.urlopen(req) as res:
+        data = res.read()
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+        print('\t- utf8 content: {}'.format(data.decode('utf8')))
