4c4c4649a35c9cd224bf63f49836293f07809023
sammyALX <samuelhailu49@gmail.com>
2022-11-09 11:49:55 +0300

    handle HTTPError response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""handle HTTPError and print status code
+"""
+
+
+if __name__ == '__main__':
+
+    from urllib.error import HTTPError, URLError
+    import urllib.request as request
+    import sys
+
+    url = sys.argv[1]
+    req_obj = request.Request(url)
+    try:
+        with request.urlopen(req_obj) as res:
+            print(res.read())
+    except HTTPError as err:
+        print("Error code: ", err.code)
