9e3f8f504b2b922c639ded773b98d61f61b9e9e4
root <samuelhailu49@gmail.com>
2023-02-09 10:56:48 -0800

    remove uncessary log to the terminal

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,7 +7,5 @@ const [filepath, message] = process.argv.slice(2, 4);
 fs.writeFile(filepath, message, { encoding: 'utf8', flag: 'w+' }, (err) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(message);
   }
 });
